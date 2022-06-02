be1d8ade0893210430ce7217bcd74aaa6332ff16
Paolo Raez <str237@gmail.com>
2022-06-02 23:34:31 +0200

    Move Summary logic todifferent file

---
+++ src/Summary.ts
@@ -0,0 +1,79 @@
+import { ATTEMPS, CharCell, CharCellStatus, DIM } from './index'
+
+interface PositionSummary {
+  exact: string
+  differentFrom: Set<string>
+}
+
+interface GameSummary {
+  blacklist: Set<string>
+  byPosition: PositionSummary[]
+}
+
+const initilizeSummary = (): GameSummary => ({
+  blacklist: new Set(),
+  byPosition: Array(DIM)
+    .fill(0)
+    .map(() => ({
+      exact: '',
+      differentFrom: new Set(),
+    })),
+})
+
+export const SummaryLogic = {
+  fromMatrix: (matrix: CharCell[][]): GameSummary => {
+    const summary = initilizeSummary()
+    for (let i of Array(ATTEMPS).keys()) {
+      for (let j of Array(DIM).keys()) {
+        const cell = matrix[i][j]
+        if (!cell.char) {
+          continue
+        }
+        switch (cell.status) {
+          case CharCellStatus.None:
+            {
+              summary.blacklist.add(cell.char)
+            }
+            break
+          case CharCellStatus.Exist:
+            {
+              summary.byPosition[j].differentFrom.add(cell.char)
+            }
+            break
+          case CharCellStatus.Exact:
+            {
+              summary.byPosition[j].exact = cell.char
+            }
+            break
+        }
+      }
+    }
+    return summary
+  },
+
+  toRegExp: (summary: GameSummary): RegExp => {
+    //regex sample: /^(?=[^g]+)(?=.*l)(?=.*a)(?=.*u)(?=[^l][^ua].[^a]r).{5}$/gm
+    const blacklist = `[^${Array.from(summary.blacklist).join('')}]`
+    const blacklistPart = `(?=${blacklist.repeat(DIM)})`
+    const mustExistSet: Set<string> = summary.byPosition.reduce((acc, pos) => {
+      for (let c of pos.differentFrom) {
+        acc.add(c)
+      }
+      return acc
+    }, new Set() as Set<string>)
+    const mustExistPart: string = Array.from(mustExistSet)
+      .map(c => `(?=.*${c})`)
+      .join('')
+    const byPositionArray = summary.byPosition.map(p => {
+      if (p.exact) {
+        return p.exact
+      } else if (p.differentFrom.size) {
+        return `[^${Array.from(p.differentFrom).join('')}]`
+      }
+      return '.'
+    })
+    const byPositionPart = `(?=${byPositionArray.join('')})`
+    const finalString = `^${blacklistPart}${mustExistPart}${byPositionPart}.{${DIM}}$`
+    return new RegExp(finalString, 'gm')
+  },
+}
