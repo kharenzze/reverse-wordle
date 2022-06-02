bca83ee2343a1142169e9afa3967c0b5a3900223
Paolo Raez <str237@gmail.com>
2022-06-02 20:42:45 +0200

    Define VERSION

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const packageJson = require('./package.json')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    VERSION: packageJson.version,
+  },
 }
 
 module.exports = nextConfig
